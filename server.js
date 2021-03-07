22151efe74d70df3cce7b2dbd86e9167f41f2312
PasinduDew <pasindudewapriya96@gmail.com>
2021-03-07 16:32:21 +0530

    Added SetTimeInterval for Testing Purpose

--- server.js
+++ server.js
@@ -5,18 +5,24 @@ const io = require("socket.io")(http);
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Ayubowan!, I'm Working");
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
+  socket.emit("init_event", { message: "A new user has joined!" });
+
   console.log(">> Client Connected");
   socket.on("disconnect", () => {
     console.log(">> Client Disconnected");
   });
 
-  socket.on('event_id', (data) => {
-    console.log('Event Data: ' + data);
+  socket.on("event_id", (data) => {
+    console.log("Event Data: " + data);
   });
+
+  setInterval(() => {
+    socket.emit("switch_01", { state: true });
+  }, 2000);
 });
 
 http.listen(PORT, () => {
