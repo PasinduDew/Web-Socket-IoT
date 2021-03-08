ee28ec3733fce7e6b3ac64be464366015e038c55
PasinduDew <pasindudewapriya96@gmail.com>
2021-03-08 07:58:33 +0530

    Added Basuc Code Required for Web Socket Server

--- server.js
+++ server.js
@@ -8,17 +8,25 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+
+// When Client Connects to the WS Server
 io.on("connection", (socket) => {
   socket.emit("init_event", { message: "A new user has joined!" });
 
   console.log(">> Client Connected");
+
+  // Client Disconnected
   socket.on("disconnect", () => {
     console.log(">> Client Disconnected");
   });
 
-  socket.on("event_id", (data) => {
-    console.log("Event Data: " + data);
-  });
+  // This is an Example Event Trigger --> event_id
+  // socket.on("event_id", (data) => {
+  //   console.log("Event Data: ", data);
+  // });
+
+  // This is an Example Event Emitter
+  // socket.emit("event_id", { message: "A new user has joined!" });
 
   setInterval(() => {
     socket.emit("switch_01", { state: true });
