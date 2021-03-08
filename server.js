b4195e2e278e961cc88b59964e510e8e22981b55
PasinduDew <pasindudewapriya96@gmail.com>
2021-03-08 13:01:49 +0530

    Test 04

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+
+const PORT = process.env.PORT || 3000;
+
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8080 });
+  , wss = new WebSocketServer({ port: PORT });
+
+  console.log("Port:", PORT);
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
