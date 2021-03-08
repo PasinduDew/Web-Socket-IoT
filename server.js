b540eea1aac00670bf3c0140ddf8d9020376e50b
PasinduDew <pasindudewapriya96@gmail.com>
2021-03-08 10:55:24 +0530

    Enabled CORS for Socket.io

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
 
 // When Client Connects to the WS Server
 io.on("connection", (socket) => {
