2661886aeea4791ba8fb0cb4522d40257d9706f7
PasinduDew <pasindudewapriya96@gmail.com>
2021-03-12 07:38:13 +0530

    test 01

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-var express = require("express");
-var app = express();
-var expressWs = require("express-ws")(app);
+const express = require("express");
+const app = express();
+const expressWs = require("express-ws")(app);
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+
+/*
+If you would rather have a list of allowed origins, you can use a function instead of a string as the origin value:
+
+*/
+// var allowedOrigins = ['http://localhost:3000',
+//                       'http://yourapp.com'];
+// app.use(cors({
+//   origin: function(origin, callback){
+//     // allow requests with no origin
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 app.use(function (req, res, next) {
   console.log("middleware");
@@ -35,4 +57,3 @@ app.ws("/", function (ws, req) {
 app.listen(PORT, () => {
   console.log(`>> Listening on ${PORT}`);
 });
-
